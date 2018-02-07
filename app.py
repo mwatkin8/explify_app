cbfacb0c655b6aca2f8f5e6d12a74d68c03aaf39
mwatkin8 <michael.watkins.8@gmail.com>
2018-02-07 11:02:55 -0700

    flask framework set up

--- app.py
+++ app.py
@@ -2,12 +2,8 @@
 templates folder.
 """
 from flask import Flask, render_template
-from fhirclient import client
 import os
-import datetime
-import fhirclient.models.observation as obs
-import fhirclient.models.patient as p
-#import fhirParser
+import fhirParser
 
 
 # create the application object
@@ -25,22 +21,6 @@ def home():
     """
     Renders index.html
     """
-    #Perform a GET on the Test siblings by id and add them to a patient dictionary
-    fhirPatients = {}
-    #fhirPatients['cf-1507072796559'] = p.Patient.read('cf-1507072796559', smart.server)
-    #fhirPatients['cf-1508283569165'] = p.Patient.read('cf-1508283569165', smart.server)
-    #fhirPatients['cf-1508283628125'] = p.Patient.read('cf-1508283628125', smart.server)
-
-    patients = []
-    for p_id,p in fhirPatients.items():
-        name = p.name[0].given[0] + p.name[0].family
-        birth = p.birthDate.isostring.split("-")
-        age = str(int(datetime.date.today().year) - int(birth[0]))
-        patients[p_id] = Patient(name, gender, age)
-
-    for patient in patients:
-        print(patient.__str__())
-
     return render_template('index.html')
 
 # start the server with the 'run()' method
